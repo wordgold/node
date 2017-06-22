16375016c8e8dd8cbce43f775c0cad940d3aa8d7
Yu Xianghui <wordgold@gmail.com>
2017-06-22 11:22:52 +0800

    Creat

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var talk = require('./talk');
+
+var t = new talk("sun");
+t.read("./tmp/t1.json")
---
+++ talk.js
@@ -0,0 +1,27 @@
+module.exports = class {
+	constructor(name) {
+		this.name = name;
+		console.log(process.cwd() + " fuck " + name + "!")
+	}
+	read(file) {
+		var fs = require("fs");
+		fs.stat(file, function(err, stat) {
+			if (err) {
+				console.log(err);
+			} else {
+				// 是否是文件:
+				console.log('isFile: ' + stat.isFile());
+				// 是否是目录:
+				console.log('isDirectory: ' + stat.isDirectory());
+				if (stat.isFile()) {
+					// 文件大小:
+					console.log('size: ' + stat.size);
+					// 创建时间, Date对象:
+					console.log('birth time: ' + stat.birthtime);
+					// 修改时间, Date对象:
+					console.log('modified time: ' + stat.mtime);
+				}
+			}
+		});
+	}
+}
